e852ffd24ca976d69f42b9ae5b964e21616f98fa
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-03-26 10:50:36 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import ListHeader from './components/ListHeader';
+
+const App = () => {
+  return (
+    <div className="app">
+      <ListHeader listName={'Holiday Tick list'}/>
+    </div>
+  );
+};
+
+export default App;
