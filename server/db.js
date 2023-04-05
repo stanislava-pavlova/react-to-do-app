222da572598126afcdf6984efaf234d457fe4b2b
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-04-05 19:07:08 +0300

    fetch data

--- server/db.js
+++ server/db.js
@@ -1,11 +1,10 @@
 const Pool = require('pg').Pool;
-require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.DBPORT,
+  user: 'postgres',
+  password: '12345',
+  host: 'localhost',
+  port: 5432,
   database: 'todoapp',
 });
 
--- server/server.js
+++ server/server.js
@@ -1,15 +1,32 @@
-const PORT = process.env.PORT ?? 8000;
+const PORT = 8000;
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const pool = require('./db');
 
+app.use(cors());
+
 // get all todos
-app.get('/todos', async (req, res) => {
+// app.get('/todos', async (req, res) => {
+//   try {
+//     const todos = await pool.query('SELECT * FROM todos');
+//     res.json(todos.rows);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// });
+
+app.get('/todos/:userEmail', async (req, res) => {
+  const { userEmail } = req.params;
   try {
-    const todos = await pool.query('SELECT * FROM todos');
+    const todos = await pool.query(
+      'SELECT * FROM todos WHERE user_email = $1',
+      [userEmail]
+    );
     res.json(todos.rows);
   } catch (err) {
     console.error(err);
+    res.status(500).send(err);
   }
 });
 
